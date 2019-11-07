0c2b7951f8bb162f842e5dc07cf756c12b820153
Cess <cessmbuguar@gmail.com>
2019-11-07 17:13:42 +0300

    model tests

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'attributes' do
+    it { should have_attribute :name }
+    it { should have_attribute :image_url}
+    it { should have_attribute :description }
+    it { should have_attribute :github_url }
+    it { should have_attribute :issue_tracker }
+    it { should have_attribute :slack_channel_name }
+    it { should have_attribute :status }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :name  }
+    it { should validate_presence_of :description }
+    it { should validate_presence_of :github_url  }
+    it { should validate_presence_of :issue_tracker  }
+    it { should validate_presence_of :status  }
+    it { should validate_presence_of :slack_channel_name }
+  end
 end

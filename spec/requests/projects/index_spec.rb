af476790514e560bf0c3f52b153cb5bede9e274a
aonomike <aonomike@gmail.com>
2020-04-21 21:02:48 +0300

    Add tests for index when there are no projects
    Co-authored-by: Riccardo Giomi <contact@riccardogiomi.com>
    Co-authored-by: Gideon Kimutai <gideon.kimutai.kim@gmail.com>
    Co-authored-by: ethanstrominger <ethanstrominger2@gmail.com>

--- spec/requests/projects/index_spec.rb
+++ spec/requests/projects/index_spec.rb
@@ -4,13 +4,11 @@ require 'rails_helper'
 
 RSpec.describe 'ProjectsController#index' do
   let(:path) { '/projects' }
-  
-  context "with 10 projects in the database" do
-    before :all do
-      create_list(:project, 10)
-    end
 
-    before :each do
+  context "with 10 projects" do
+  
+    before :example do
+      create_list(:project, 10)
       get(path)
     end
 
@@ -23,6 +21,18 @@ RSpec.describe 'ProjectsController#index' do
     end
   end
 
-  context '' do
+  context 'with no projects' do
+
+    before :example do
+      get(path)
+    end
+
+    it 'responds with status code 200' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns an empty array' do
+      expect(response.parsed_body).to be_blank
+    end
   end
 end

c446e0421a90e7f8280b91b61da66e4b075244f8
aonomike <aonomike@gmail.com>
2020-04-22 21:10:21 +0300

    Implement test for returnig good result
    Co-authored-by: Riccardo Giomi <contact@riccardogiomi.com>
        Co-authored-by: ethanstrominger <ethanstrominger2@gmail.com>
        Co-authored-by: Gideon Kimutai <gideon.kimutai.kim@gmail.com>

--- spec/requests/projects/show_spec.rb
+++ spec/requests/projects/show_spec.rb
@@ -1,22 +1,26 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+
 RSpec.describe 'ProjectsController#show' do
   context 'with valid parameters' do
-    let(:project) do
-      create(:project, title: 'Amazing project',
-                       status: 'Active',
-                       description: 'Amazing project description')
+    let(:project_attributes) do
+      {
+        title: 'Amazing project',
+        status: 'Active',
+        description: 'Amazing project description'
+      }
     end
-
+    let(:project) { create(:project, project_attributes) }
     let(:path) { project_path(project) }
+    before(:example) { get(path) }
 
     it 'responds with status code 200' do
-      get(path)
       expect(response.status).to eq 200
     end
 
     it 'returns the correct result' do
+      expect(response.parsed_body.deep_symbolize_keys!).to include(project_attributes)
     end
   end
 end

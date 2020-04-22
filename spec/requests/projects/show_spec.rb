b07d7706f0a5500881c0b40cfed81043935a150d
aonomike <aonomike@gmail.com>
2020-04-22 19:17:36 +0300

    Add request spec for ProjectController#show
    Co-authored-by: Riccardo Giomi <contact@riccardogiomi.com>
        Co-authored-by: Gideon Kimutai <gideon.kimutai.kim@gmail.com>
        Co-authored-by: ethanstrominger <ethanstrominger2@gmail.com>

---
+++ spec/requests/projects/show_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+RSpec.describe 'ProjectsController#show' do
+  context 'with valid parameters' do
+    let(:project) do
+      create(:project, title: 'Amazing project',
+                       status: 'Active',
+                       description: 'Amazing project description')
+    end
+
+    let(:path) { project_path(project) }
+    
+    it 'responds with status code 200' do
+      get(path)
+      expect(response.status).to eq 200
+    end
+
+    it 'returns the correct result' do
+    end
+  end
+end
